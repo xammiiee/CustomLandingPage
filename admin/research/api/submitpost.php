3c6d8b8b554998bb1716d4e6d7cbdaeed2935861
Jaren <traildownhill@gmail.com>
2022-03-19 23:03:46 +0800

    UPDATE

--- admin/research/api/submitpost.php
+++ admin/research/api/submitpost.php
@@ -29,8 +29,35 @@
         $dpub =$_POST['dpub'];
         $fstudy = $_POST['fstudy'];
     
-        //for logs
-        
+        // UPLOADING PDF FILE
+        if( isset( $_FILES['pdfFile'] ) ) {
+            if ($_FILES['pdfFile']['type'] == "application/pdf") {
+                $source_file = $_FILES['pdfFile']['tmp_name'];
+                $dest_file = "../uploads/".$_FILES['pdfFile']['name'];
+                if (file_exists($dest_file)) {
+                    print "The PDF file name already exists!!";
+                }
+                else {
+                    move_uploaded_file( $source_file, $dest_file )
+                    or die ("Error!!");
+                    if($_FILES['pdfFile']['error'] == 0) {
+                        print "<b><u>Details : </u></b><br/>";
+                ?>
+                <input hidden type="text" name="pdffile" id="pdffile" class="form-control" value="<?php echo "../papers/uploads/".$_FILES['pdfFile']['name']?> " >
+                <?php
+                echo "File Name : ".$_FILES['pdfFile']['name']."<br.>"."<br/>";
+                        echo "File location : ../uploads/".$_FILES['pdfFile']['name']."<br/>";
+                    }
+                }
+            }
+            else {
+                if ( $_FILES['pdfFile']['type'] != "application/pdf") {
+                    print "Error occured while uploading file : ".$_FILES['pdfFile']['name']."<br/>";
+                    print "Invalid  file extension, should be pdf !!"."<br/>";
+                    print "Error Code : ".$_FILES['pdfFile']['error']."<br/>";
+                }
+            }
+        }
         
         // UPLOAD TO TBL-RESEARCH
             $result = mysqli_query($con, "INSERT INTO tblresearch (`id`, `title`, `abstract`, `main_author`, `co_authors`, `date_publish`, `field_of_study`, `department`, `pdf_file`, `views`, `cites`, `tagging`) VALUES  ('$id', '$title', '$abs', '','','$dpub','$fstudy','','$pdf_file','','','$tags')");
