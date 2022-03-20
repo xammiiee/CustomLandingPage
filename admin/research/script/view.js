530d42339fe5c4718bba7886f6109ca78dc18237
Jaren <traildownhill@gmail.com>
2022-03-20 16:14:02 +0800

    update

--- admin/research/script/view.js
+++ admin/research/script/view.js
@@ -1,17 +1,16 @@
 $(document).ready(function($){
   $('body').on('click', '.view', function () {
     var id = $(this).data('id');
+    var pdf_file = $(this).data('pdf_file');
     // var 
     // ajax
     $.ajax({
     type:"POST",
-    url: "ajax-fetch-record.php",
-    data: { id: id },
+    url: "../api/action.php",
+      data: { id: id, pdf_file: pdf_file },
     dataType: 'json',
     success: function(res){
       $('#fname').html(res.fname);
-      $('#lname').html(res.lname);
-      $('#email').html(res.email);
       }
     });
   });
