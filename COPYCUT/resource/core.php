7c3e7077b163aba34c17d908c8dad5b757a104e4
erllbryan <iambryanmalindog@gmail.com>
2022-03-24 12:51:44 +0800

    update
    copycut

---
+++ COPYCUT/resource/core.php
@@ -0,0 +1,6 @@
+<?php
+include "../../COPYCUT/config/db.php";
+include "../../COPYCUT/functions/DB.func.php";
+include "../../COPYCUT/functions/functions.php";
+include "../../COPYCUT/functions/Message.func.php";
+?>
