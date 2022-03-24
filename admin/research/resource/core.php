46f7dfd12248406ce1858a92870b760c9b03454f
Jaren <traildownhill@gmail.com>
2022-03-24 23:39:05 +0800

    clean up

--- admin/research/resource/core.php
+++
@@ -1,6 +0,0 @@
-<?php
-include "../../COPYCUT/config/db.php";
-include "../../COPYCUT/functions/DB.func.php";
-include "../../COPYCUT/functions/functions.php";
-include "../../COPYCUT/functions/Message.func.php";
-?>
