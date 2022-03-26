08a5929ffb83e62d387e22ce9ab249aefb58689d
traildownhill <75106317+traildownhill@users.noreply.github.com>
2022-03-26 16:15:39 +0800

    Delete core.php

--- admin/journal/core.php
+++
@@ -1,6 +0,0 @@
-<?php
-include("{$_SERVER['DOCUMENT_ROOT']}/CustomLandingPage/config/config.php");
-include("{$_SERVER['DOCUMENT_ROOT']}/CustomLandingPage/admin/journal/functions/DB.func.php");
-include("{$_SERVER['DOCUMENT_ROOT']}/CustomLandingPage/admin/journal/functions/functions.php");
-include("{$_SERVER['DOCUMENT_ROOT']}/CustomLandingPage/admin/journal/functions/Message.func.php");
-?>
