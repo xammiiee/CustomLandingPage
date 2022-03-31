e0e9c5fd3507508e1c91248a3856b413678451f9
erllbryan <iambryanmalindog@gmail.com>
2022-03-31 10:58:23 +0800

    author

--- admin/author/api/action.php
+++ admin/author/api/action.php
@@ -1,48 +1,45 @@
 <?php
-include_once "./resource/inc/header.php";
+include_once "../inc/header_1.php";
 if (empty($_SESSION['id'])) {
 	// include""
-	header("Location: ./login/login.php");
+	
 }
 ?>
 
 <!--View Author-->
 <?php 
-//updating of Research
+//updating of author
 if (isset($_POST['id'])) {
-	// change function to the designated function of your assign management
-	// also correct each string of the sql with your form
- 		$result = update_journalaction($connect,$_POST['name'],$_POST['email'],$_POST['profession'],$_POST['description'],$_POST['id'],$_POST['created'],$filelocation,"0");
+	
+ 		$result = update_authoraction($connect,$_POST['name'],$_POST['profession'],$_POST['fstudy'],$_POST['description'],$_POST['id']);
  		if ($result == "1") {
 			echo'<div style="position:relative;top: 100px;"';
- 			message("Research updated successfully!",1);
+ 			message("Author updated successfully!",1);
  		}
  	}
-// deleting of journal
+// deleting of author
  if (isset($_GET['del'])) {
-	//  change function to the designated function of your assign management
- 	$result = delete_journalaction($connect,$_GET['del']);
+
+ 	$result = delete_authoraction($connect,$_GET['del']);
  	if ($result =="1") {
-		//  change location to the page of your assign mangement
- 		header("Location: ./admin/research/research.php");
- 		message("Research deleted successfully!","1");
+		
+ 		header("Location: action.php");
+ 		message("Author deleted successfully!","1");
  	}
  }
 
-// editing of Research
+// editing of author
 if (isset($_GET['edit'])) {
-	// change function to the designated function of your assign management
-	$data = get_journalaction($connect,$_GET['edit']);
+	$data = get_authors($connect,$_GET['edit']);
 	?>
 	<br><br><br><br>
 	<div class="container">
-		<table class="table table-bordered " >
+		<table class="table table-bordered" style="width:80%;margin:auto;">
 			<thead class="thead-dark">
-			
 				<tr>
 					<th scope="cols" colspan="3" class="p-0">
 						<!--  -->
-						<h5> <a href="action.php?id=<?php echo $data['id'];?>&ref=journal"><button class="btn btn-dark btn-sm">← Back to project</button></a> </h5>
+						<h5> <a href="action.php?id=<?php echo $data['id'];?>&ref=author"><button class="btn btn-dark btn-sm">← Back to project</button></a> </h5>
 					</th>
 				</tr>
 			</thead>
@@ -52,29 +49,26 @@ if (isset($_GET['edit'])) {
 					<td>
 						<!-- change this form to what must be edited to your assign management -->
 						<div class="form-group">
-							<label for="author">Author Name</label>
-							<input type="text" class="form-control" id="author" name="author" value="<?php echo $data['author'];?>">
+							<label for="name">Author Name</label>
+							<input type="text" class="form-control" id="name" name="name" value="<?php echo $data['name'];?>">
 							<div class="form-group">
-								<label for="title">Title</label>
-								<input class="form-control" id="title" name="title" value="<?php echo $data['title'];?>">
+								<label for="profession">Profession</label>
+								<input class="form-control" id="profession" name="profession" value="<?php echo $data['profession'];?>">
+							</div>
+							<div class="form-group">
+								<label for="fstudy">Field of Study</label>
+								<input class="form-control" id="fstudy" name="fstudy" value="<?php echo $data['fstudy'];?>">
 							</div>
 							<div class="form-group">
 								<label for="description">Description</label>
 								<textarea class="form-control" id="description" name="description" rows="10"></textarea>
 							</div>
 						</div>
-						<div class="form-group">
-							<label for="datepub">Date Publish</label>
-							<input type="date" class="form-control" id="datepub" name="datepub" value="<?php echo $data['datepub'];?>">
-						</div>
-
 						<input type="hidden" class="form-control" id="id" name="id" value="<?php echo $data['id'];?>">
-
 						<div class="form-group" align="right">
-							<button class="btn btn-primary btn-sm">Save Project</button> <a class="btn btn-dark btn-sm" href="/project.php?id=<?php echo $data['id'];?>&ref=journal">Cancel</a>
+							<button class="btn btn-primary btn-sm">Save Project</button>
 						</div>
 					</td>
-
 				</tr>
 			</tbody>
 		</form>
@@ -83,52 +77,85 @@ if (isset($_GET['edit'])) {
 		</div>
 
 <?php } ?>
-
 <?php 
-
-// Get Research id
 if (!empty($_GET['id'])) {
-	// change function to the designated function of your assign management
-	$data = get_journal($connect,$_GET['id']);
+	$data = get_authors($connect,$_GET['id']);
 	?>
 <br><br><br><br>
 	<!--View Research-->
-	<div class="container">
-	<div class="card">
-		<div class="card-body">
-		<div style="line-height: 20px;">
-		<style>
-			h2,p{
-				padding:0;
-				margin:0;
-			}
-			/* .card{
-
-			} */
-		</style>
-					<div class="badge badge-info text-wrap" style="width: 4rem;padding:5px;" >
-					<!-- change to.. -->
-					<span >Research</span>
-					</div>
-					<h2 class="text-left" style="margin-top:10px;" ><?php echo $data['title']?></h2>
-						<button class="btn btn-primary btn-sm float-right" style="position:relative;bottom:40px;" ><i class="fa fa-download"> Download fulltext PDF&nbsp;</i></button>
-						<button class="btn btn-outline-primary btn-sm float-right" style="position:relative;bottom:40px;" ><i class="fa fa-file-text"> Download fulltext PDF&nbsp;</i></button>
-							
-						
-
-					<p class="font-weight-normal text-left" style="width:50%;"><?php echo ($data['description']); ?></p>
-					<p class="font-weight-sm-light text-left" style="font-size: 15px;margin-top:10px;" ><?php echo date("Y-m-d",strtotime($data['datepub']));?></p>
-					<p style="margin-top:10px;"><b>Authors:</b></p>
-					<p style="margin-top:10px;"><a href=""><?php echo $data['author']?></a></p>
-					</div>
-					</div>
-					</div>
+	<div class="container" >
+        <div class="card">
+         <div class="card-body">
+			<style>
+				body{background-color:#f4f4f4;}h4,p{margin: 0;padding:0;}
+				p{font-weight:600px;font-size:14px;}
+				</style>
+	 		<div class="container" ">
+			 	<div class="text-left">
+              		<div class="row">
+						<div class="col-2">
+						<img src="/CustomLandingPage/admin/profile/uploads/ayanokoji.jpg" class="rounded-circle w-cover" alt="..." width="100"   >
+						</div>
+						<div style="position:relative;right:40px;top:15px;font-weight:800px;">
+							<h4 style="margin-bottom:5px;font-size:30px;">
+							<?php echo $data['name']?></h4>
+							<p class="text-muted"><?php  echo $data['fstudy'];?> &nbsp;&#x22C5;&nbsp;<?php echo  date("F Y",strtotime($data['created']));?></p>
+							<p class="text-muted"><?php  echo $data['profession'];?></p>
+						</div>
+                	</div>
+            	</div>
+        	 </div>
+			 <div class="card-body " >
+					<ul class="nav nav-tabs" id="myTab" role="tablist">
+						<li class="nav-item" role="presentation">
+							<a class="nav-link active" id="about-tab" data-toggle="tab" href="#about" role="tab" aria-controls="about" aria-selected="true">About</a>
+						</li>
+						<li class="nav-item" role="presentation">
+							<a class="nav-link" id="research-tab" data-toggle="tab" href="#research" role="tab" aria-controls="research" aria-selected="false">Research</a>
+						</li>
+					</ul>
+						<div class="tab-content " id="myTabContent">
+								<div class="tab-pane fade show active  " id="about" role="tabpanel" aria-labelledby="about-tab" >
+									<h3 class="text-muted">About</h3>
+									<!-- <div class="card"> -->
+										<!-- <div class="card-body"> -->
+											<div class="row">
+												<div class="col-sm-6">
+													<h5 class="card-title text-muted">Research</h5>
+													<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+												</div>
+												<div class="col-sm-6">
+													<h5 class="card-title text-muted">Views</h5>
+													<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+												</div>   
+											</div>
+										<!-- </div> -->
+									<!-- </div> -->
+									<div class="card-body"></div>
+									<div class="row">
+										<div class="col-sm">
+											<!-- <div class="card"> -->
+												<!-- <div class="card-body"> -->
+													<h5 class="text-muted">Intro</h5>
+													<hr>
+														<p class="card-text"><?php echo $data['description'];?></p> 
+												</div>
+											<!-- </div> -->
+										<!-- </div> -->
+									</div>
+									</div>
+								<div class="tab-pane fade" id="research" role="tabpanel" aria-labelledby="research-tab">...</div>
+							</div>
+          		</div>
+		</div>
+    </div>
+ </div>						
 	<br<br><br>
 	<div class="container">
 		<div id="result"></div>
 			<div class="modal-footer">
 				<!-- change location of href..-->
-				<a href="./admin/research/research.php"><button class="btn btn-dark btn-sm">Back</button></a>
+				<a href="../author.php"><button class="btn btn-dark btn-sm">Back</button></a>
 					<div class="dropdown">
 						<button class="btn btn-light btn-sm" type="button" id="option" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 							<i class="fa fa-ellipsis-h"></i>
