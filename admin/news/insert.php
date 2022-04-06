85fa804685e7d8d3cbf25f6504bb5435f3827bc1
traildownhill <cirejmalapad@gmail.com>
2022-04-06 21:37:52 +0800

    w index 2 / insert news management

---
+++ admin/news/insert.php
@@ -0,0 +1,35 @@
+
+<?php
+//insert.php
+
+if(isset($_POST["name"]))
+{
+    
+    $email = $_POST['email'];
+    $mobile = $_POST['mobile'];
+    $author = $_POST['author'];
+
+ $connect = new PDO("mysql:host=localhost;dbname=research_portal", "root", "");
+ $query = "INSERT INTO tblnews(name,email,mobile,author,tags) VALUES(:name,'$email','$mobile','$author' :skill)";
+ $statement = $connect->prepare($query);
+ $statement->execute(
+  array(
+   ':name'  => $_POST["name"],
+   ':skill' => $_POST["skill"]
+  )
+ );
+
+ $result = $statement->fetchAll();
+ $output = '';
+ if(isset($result))
+ {
+  $output = '
+  <div class="alert alert-success">
+   Your data has been successfully saved into System
+  </div>
+  ';
+ }
+ echo $output;
+}
+
+?>
