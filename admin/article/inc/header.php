803ba3fbb55194f8fa0ee13b34a748c49097ce31
erllbryan <iambryanmalindog@gmail.com>
2022-04-09 18:22:55 +0800

    update

--- admin/article/inc/header.php
+++ admin/article/inc/header.php
@@ -103,10 +103,6 @@ include "/xampp/htdocs/CustomLandingPage/admin/article/functions/functions.php";
                   <?php } ?>
 
                   <?php if ($_SESSION['role'] == "Administrator") { ?>
-                  <a class="dropdown-item" href="../author/author.php">Author</a>
-                  <?php } ?>
-
-                  <?php if ($_SESSION['role'] == "Administrator") { ?>
                   <a class="dropdown-item" href="../events/index.php">Events</a>
                   <?php } ?>
 
