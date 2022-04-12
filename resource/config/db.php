1429f3b188457c0c638f3a9799cb25fbcb77c07a
Jaren <traildownhill@gmail.com>
2022-04-12 21:09:30 +0800

    Update db.php

--- resource/config/db.php
+++ resource/config/db.php
@@ -1,17 +1,6 @@
 <?php
 	session_start();
-	$con = mysqli_connect("remotemysql.com","HV9Jv6X0OX","1mlYPFrQoR","HV9Jv6X0OX");
-	
-	if (mysqli_connect_errno())
-	{
-	  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-	  die();
-  	}
-	  else{
-		//   echo "Success";
-	}
-
-	// $con = mysqli_connect("localhost","root","","research_portal");
+	// $con = mysqli_connect("remotemysql.com","HV9Jv6X0OX","1mlYPFrQoR","HV9Jv6X0OX");
 	
 	// if (mysqli_connect_errno())
 	// {
@@ -19,6 +8,17 @@
 	//   die();
   	// }
 	//   else{
-	// 	  echo "Success";
-	//   }
+	// 	//   echo "Success";
+	// }
+
+	$con = mysqli_connect("localhost","root","","research_portal");
+	
+	if (mysqli_connect_errno())
+	{
+	  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	  die();
+  	}
+	  else{
+		  echo "Success";
+	  }
 ?>
